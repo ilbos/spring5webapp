49f5a8103548df35e63f62b8e3cf8cb5d419effb
Ilgar Bosatov <ilgar.bosatov@gmail.com>
2020-10-03 23:56:14 +0200

    added author to Book model

--- src/main/java/bi/springframework/spring5webapp/domain/Book.java
+++ src/main/java/bi/springframework/spring5webapp/domain/Book.java
@@ -1,16 +1,20 @@
 package bi.springframework.spring5webapp.domain;
 
+import java.util.Set;
+
 public class Book {
 
     private String title;
     private String isbn;
+    private Set<Author> authors;
 
     public Book() {
     }
 
-    public Book(String title, String isbn) {
+    public Book(String title, String isbn, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
+        this.authors = authors;
     }
 
     public String getTitle() {
@@ -27,5 +31,13 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 }
