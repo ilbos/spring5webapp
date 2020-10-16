17e2a49c2f2cf6b3741a8d5b4f4f1d6c9822a5e4
Ilgar Bosatov <ilgar.bosatov@gmail.com>
2020-10-16 23:51:04 +0200

    add publisher to BootStrapData

--- src/main/java/bi/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/bi/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package bi.springframework.spring5webapp.bootstrap;
 
 import bi.springframework.spring5webapp.domain.Author;
 import bi.springframework.spring5webapp.domain.Book;
+import bi.springframework.spring5webapp.domain.Publisher;
 import bi.springframework.spring5webapp.repository.AuthorRepository;
 import bi.springframework.spring5webapp.repository.BookRepository;
+import bi.springframework.spring5webapp.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,14 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,17 +46,18 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher fantasy = new Publisher();
+        fantasy.setAddressLine1("Krefeld");
+        fantasy.setCity("Krefeld");
+        fantasy.setName("fantasy");
+        fantasy.setState("NRW");
+        fantasy.setZip("47799");
+        publisherRepository.save(fantasy);
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
-        System.out.println("Number of books: " + authorRepository.count());
-
-
-        //System.out.println("Number of books: " + authorRepository.findAll());
-/*
-
-        for (Book books : bookRepository.findAll()) {
-            System.out.println("Books is " + books);
-        }*/
+        System.out.println("Number of authors: " + authorRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
 
     }
 }
